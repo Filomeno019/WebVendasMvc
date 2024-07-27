1944ad059592b805330316c779c8442c94af0cea
Antonio Filomeno <antoniofilomeno019@gmail.com>
2024-07-27 19:36:00 -0300

    integridade referencial foreikey

--- WebVendasMvc/Models/Seller.cs
+++ WebVendasMvc/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace WebVendasMvc.Models
         public double BaseSalary { get; set; }
         public DateTime BirthDate { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
@@ -27,6 +28,7 @@ namespace WebVendasMvc.Models
             BirthDate = birthDate;
             BaseSalary = baseSalary;
             Department = department;
+
         }
 
         public void AddSales(SalesRecord sr)
