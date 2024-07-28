405664c8210700106f119a638432c26f528cdc6f
Antonio Filomeno <antoniofilomeno019@gmail.com>
2024-07-28 14:45:43 -0300

    Detalhamento do vendedor, construcao

--- WebVendasMvc/Services/SellerService.cs
+++ WebVendasMvc/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebVendasMvc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebVendasMvc.Services
 {
@@ -29,7 +30,8 @@ namespace WebVendasMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id) 
